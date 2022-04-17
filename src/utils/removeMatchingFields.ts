6734b46b5994bb7c5fee5519b6b795522cf5a0ab
Renato Sinohara <rsinohara@hotmail.com>
2022-04-17 11:42:17 +0200

    Make sure item selection works if deleted or added

--- src/utils/removeMatchingFields.ts
+++ src/utils/removeMatchingFields.ts
@@ -2,6 +2,13 @@ import _ from "lodash";
 
 export const removeMatchingFields = (mainObject: {[k: string]: any}, otherObject: {[k: string]: any}): object => {
 
+  if (!mainObject ) {
+    return otherObject;
+  }
+
+  if (!otherObject) {
+    return mainObject;
+  }
   const allKeys = [...Object.keys(mainObject), ...Object.keys(otherObject)]
   return allKeys.reduce((acc, currKey) =>
     _.isEqual( mainObject[currKey], otherObject[currKey]) ? acc : { ...acc, [currKey]: mainObject[currKey] }
